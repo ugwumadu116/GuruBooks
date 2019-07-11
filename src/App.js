3b461be5fdf0f01328004f0376a67755b04b372c
ugwumadu116 <ugwumadu116@gmail.com>
2019-07-11 15:43:13 +0100

    created new react file for listing movies

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import CounterApp from './components/counterAppComponet';
+
+function App() {
+  return (
+    <div>
+      <CounterApp />
+    </div >
+  );
+}
+
+export default App;
